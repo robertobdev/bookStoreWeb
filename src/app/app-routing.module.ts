dcdf10b5b301457fa867341eab7c02e390e7bf23
Roberto <robertojf95@gmail.com>
2019-11-17 16:24:13 -0300

    create router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path: 'books',
+  //   loadChildren: () => import('./modules/books/books.module').then(mod => mod.BooksModule)
+  // },
+  // {
+  //   path: '**',
+  //   redirectTo: 'books'
+  // }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/modules/books/books-routing.module.ts
+++ src/app/modules/books/books-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BooksListComponent } from './books-list/books-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path: '',
+  //   component: BooksListComponent
+  // }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
