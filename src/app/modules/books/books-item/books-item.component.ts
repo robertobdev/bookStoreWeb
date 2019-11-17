4b03ce06a88840330faffb6f9fb391c62c3d5595
Roberto <robertojf95@gmail.com>
2019-11-17 16:52:25 -0300

    create templater

--- src/app/modules/books/books-item/books-item.component.ts
+++ src/app/modules/books/books-item/books-item.component.ts
@@ -11,5 +11,14 @@ export class BooksItemComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  //books 
+  /**
+  {
+    'id': 1,
+    'price': 35.00,
+    'title': 'O Senhor dos Anéis: A Sociedade do Anel',
+    'author': 'J.R.R. Tolkien',
+    'image': 'https://images-na.ssl-images-amazon.com/images/I/81SWBRKfExL.jpg'
+  },
+   */
 }
